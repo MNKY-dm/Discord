4a71fa64e28b5a367d98d0337be5db396e05a308
kiverix <137424284+Kiverix@users.noreply.github.com>
2025-04-08 16:20:57 +0200

    Update discord_bdd.sql

--- discord_bdd.sql
+++ discord_bdd.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:8889
--- Généré le : dim. 06 avr. 2025 à 11:00
--- Version du serveur : 8.0.40
--- Version de PHP : 8.3.14
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2025 at 04:19 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,170 +18,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `discord_bdd`
+-- Database: `discord_bdd`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `channels`
---
-
-CREATE TABLE `channels` (
-  `channel_id` int NOT NULL,
-  `channel_name` varchar(250) NOT NULL,
-  `server_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `member`
---
-
-CREATE TABLE `member` (
-  `server_id` int NOT NULL,
-  `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `messages`
---
-
-CREATE TABLE `messages` (
-  `message_id` int NOT NULL,
-  `message_content` varchar(250) NOT NULL,
-  `channel_id` int NOT NULL,
-  `message_date` timestamp(6) NOT NULL,
-  `sender_id` int NOT NULL,
-  `receiver_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `server`
---
-
-CREATE TABLE `server` (
-  `server_id` int NOT NULL,
-  `server_name` varchar(250) NOT NULL,
-  `admin_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
-  `user_id` int NOT NULL,
-  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
-(2, 'mnky', 'dylan.moulin@gmail.com', '$2y$10$.ER2f0/IUc4Wy7KFXPa7ZuJlIKdUXAzNI4xE6ryPDl54wkAhSYWVe');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `channels`
---
-ALTER TABLE `channels`
-  ADD PRIMARY KEY (`channel_id`),
-  ADD KEY `server_id` (`server_id`);
-
---
--- Index pour la table `member`
---
-ALTER TABLE `member`
-  ADD KEY `server_id` (`server_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Index pour la table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`message_id`),
-  ADD KEY `channel_id` (`channel_id`),
-  ADD KEY `receiver_id` (`receiver_id`),
-  ADD KEY `sender_id` (`sender_id`);
-
---
--- Index pour la table `server`
---
-ALTER TABLE `server`
-  ADD PRIMARY KEY (`server_id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `channels`
---
-ALTER TABLE `channels`
-  MODIFY `channel_id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `message_id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `server`
---
-ALTER TABLE `server`
-  MODIFY `server_id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `channels`
---
-ALTER TABLE `channels`
-  ADD CONSTRAINT `channels_ibfk_1` FOREIGN KEY (`server_id`) REFERENCES `server` (`server_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Contraintes pour la table `member`
---
-ALTER TABLE `member`
-  ADD CONSTRAINT `member_ibfk_1` FOREIGN KEY (`server_id`) REFERENCES `server` (`server_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `member_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Contraintes pour la table `messages`
---
-ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`channel_id`) REFERENCES `channels` (`channel_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `messages_ibfk_3` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(18, 'Kiverix', 'edouard@mail.europe', 'mot_de_passe_securise_123456'),
+(19, 'Skyrim', 'criminal.scum@skyrim.game', 'youHaveBrokenTheLaw'),
+(0, 'le_gov_frc', 'gov@gov.gov', 'el_gobierno.gov'),
+(78, 'region_idf', 'iledefrance@europe.unioneuro', 'jaimeleurope121212'),
+(9, 'HorsLaLoi', 'crimedanslesang@prison.libre', 'jesuishorslaloimdr'),
+(3, 'Negan', 'lemecdansTWD@oui.non', 'jaimembattedebaseball'),
+(445, 'LaPireDentitionDeurope', 'dentirion.nulle@miam.com', 'jeSuisLhommeAvecLaPireDentitionDeurope'),
+(798, 'Morgan_Yu', 'morgan.yu@transtar.co', 'FParad0x?'),
+(1, 'michel', 'michel.saumon@gmail.com', '1_897p_sr87&&sh3_'),
+(999, 'BigMan', 'big.man@huge.guy', 'ITSABIGONE');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
