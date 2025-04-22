0af96d6abb25ec6a40d6a10edfc3366cfcf9a550
Dylan <dylan.moulin9272@gmail.com>
2025-04-22 17:54:21 +0200

    Amélioration de la classe, addmember...

--- code/class/Server.php
+++ code/class/Server.php
@@ -1,22 +1,27 @@
 <?php
+require_once '../bdd.php';
+
 class Server 
 {
 
     public $id;
     public $name;
     public $creator_id;
-    public $member_list;
+    public array $member_list;
 
-    public function __construct (int $server_id, string $server_name, int $creator_id) 
+    public function __construct (string $server_name, int $creator_id) 
     {
-        $this->id = $server_id;
         $this->name = $server_name;
         $this->creator_id = $creator_id;
-        $this->member_list = [];
+        $this->member_list[] = $creator_id;
     }
 
     public function addMember (int $member_id) {
+        $this->member_list[] = $member_id;
+    }
 
+    public function getMembers () {
+        return $this->member_list;
     }
 
 }
