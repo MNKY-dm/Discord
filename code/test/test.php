3cfe18a1fc6b55e69f756584f2de10988fd481ca
Dylan MOULIN <d.moulin@allianceautomotive.fr>
2025-04-23 12:46:46 +0200

    suppresion du dossier test

--- code/test/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Server.php';
-
-$server1 = new Server(1, 'Serveur 1');
-$server2 = new Server(2, 'Serveur 2');
-
-var_dump($server1, $server2);
