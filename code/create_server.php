17d43e3964f8a21eb6c1d03a4a451416995a5ef6
Dylan <dylan.moulin9272@gmail.com>
2025-04-28 16:10:00 +0200

    Ajout du formulaire HTML dans le fichier create_server.php

---
+++ code/create_server.php
@@ -0,0 +1,6 @@
+<form action="create_server.php" method="post">
+    <label for="server_name">
+        Nommez votre serveur : 
+        <input type="text" name="server_name" id="server_name" placeholder="Nom du serveur">
+    </label>
+</form>
