e376cb78e0ae6d27825d04c59a4afd7bcc571408
Dylan <dylan.moulin9272@gmail.com>
2025-04-28 21:55:12 +0200

    Implémentation propre des pages success et error dans le fichier create_server

--- code/discord_server/create_server.php
+++ code/discord_server/create_server.php
@@ -3,15 +3,21 @@ require "create_server_form.php";
 require "class" . DIRECTORY_SEPARATOR . "Server.php";
 require ".." . DIRECTORY_SEPARATOR . "bdd.php";
 
-if ($_POST['server_name'] !== "") {
-    $server_name = htmlentities($_POST['server_name']);
-    if (Server::createServer($conn, $server_name, 1) !== null) {
+$server_name = htmlentities($_POST['server_name']) ?? null;
+$creator_id = 1; // À automatiser via la session utilisateur
+
+if ($server_name) {
+    $new_server = Server::createServer($conn, $server_name, $creator_id) ;
+    if ($new_server) {
         header('Location: success.php');
+        exit;
     } else {
         header('Location: error.php');
+        exit;
     }
 } else {
-    echo "Le nom du serveur ne peut pas être vide. Veuillez réessayer.";
+    header('Location: error.php');
+    exit;
 }
 
 
