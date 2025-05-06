0dc43e53dfb58bf1489e6d1d5b741270093f66f8
Dylan <dylan.moulin9272@gmail.com>
2025-05-06 10:59:24 +0200

    test

---
+++ code/server-bar.php
@@ -0,0 +1,19 @@
+<div class="server-bar">
+    <div class="private-messages">
+        <a href="/code/index.php">
+            <img src="https://placehold.co/40" alt="Espace privé">
+        </a>
+    </div>
+    <div class="separator"><div class="server-separator"></div></div>
+    <div class="servers">
+        <img src="https://placehold.co/40" alt="Serveur 1">
+        <img src="https://placehold.co/40" alt="Serveur 2">
+        <img src="https://placehold.co/40" alt="Serveur 3">
+        <img src="https://placehold.co/40" alt="Serveur 4">
+        <img src="https://placehold.co/40" alt="Serveur 5">
+        <a href="discord_server/create_server_form.php">
+            <img src="https://placehold.co/40" alt="Ajouter un serveur">
+        </a>
+        <img src="https://placehold.co/40" alt="Découvrir">
+    </div>
+  </div>
---
+++ code/template_server.php
@@ -0,0 +1,10 @@
+<?php
+include("header.php");
+include("server-bar.php");
+include("channels-bar.php");
+echo $content;
+
+?>
+
+</body>
+</html>
