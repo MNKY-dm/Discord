6278fa0c6aa853f733a68705e16b3a294986250a
Dylan <dylan.moulin9272@gmail.com>
2025-05-08 23:10:15 +0200

    AJAX -> Récupérer les infos des channels en cliquant sur un serveur dans server-bar.php

--- code/server-bar.php
+++ code/server-bar.php
@@ -1,3 +1,9 @@
+<?php
+require_once('bdd.php');
+$servers = $conn->query("SELECT server_id, server_name FROM server")->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <div class="server-bar">
     <div class="private-messages">
         <a href="/Discord/code/index.php">
@@ -6,15 +12,38 @@
     </div>
     <div class="separator"><div class="server-separator"></div></div>
     <div class="servers">
-        <img src="https://placehold.co/40" alt="Serveur 1">
-        <img src="https://placehold.co/40" alt="Serveur 2">
-        <img src="https://placehold.co/40" alt="Serveur 3">
-        <img src="https://placehold.co/40" alt="Serveur 4">
-        <img src="https://placehold.co/40" alt="Serveur 5">
-        <a href="discord_server/create_server_form.php">
+        <?php foreach ($servers as $server) : ?>
+            <img src="https://placehold.co/40" alt="<?= htmlspecialchars($server["server_name"]) ?> " data-server-id="<?= $server['server_id'] ?>">
+        <?php endforeach; ?>
+        <script>
+            document.querySelectorAll("[data-server-id]").forEach(img => { // Sélectionne les éléments qui ont l'attribut 'data-server-id'
+
+                img.addEventListener('click', (event) => { // Pour chaque img trouvée, ajoute un événement "click"
+
+                    const serverId = event.currentTarget.dataset.serverId // Récupère la valeur de 'data-server-id' en type str grâce à dataset
+                    console.log('Serveur cliqué :', serverId) // Affiche le résultat dans la console du navigateur pour vérifier si cela fonctionne
+                    fetch("./discord_server/get_channels.php?server_id=" + serverId)  // Envoie une requête http asynchrone ('fetch()') avec l'id du serveur qui convient depuis le fichier server.php
+                        .then(response => { // Dès que la requête reçoit une réponse, les données sont récupérées en format json (pour rendre les données compatibles entre JS et PHP)
+                            if (!response.ok) {
+                                console.log(response)
+                                throw new Error('HTTP error ' + response.status);
+                            }
+                            return response.json();
+                        })
+                        .then(data => { // Ensuite, affiche les données dans la console pour vérifier une fois de plus si ça fonctionne
+                            console.log('Données reçues :', data)
+                        })
+
+                        .catch(error => { // Si le fecth ne fonctione pas, attrape une erreur
+                            console.error('Erreur AJAX :', error)
+                        })
+                })
+            })
+        </script>
+        <a href="/Discord/code/discord_server/create_server_form.php">
             <img src="https://placehold.co/40" alt="Ajouter un serveur">
         </a>
-        <a href="/code/deconnexion.php">
+        <a href="/Discord/code/deconnexion.php">
             <img src="https://placehold.co/40" alt="Se déconnecter">
         </a>
     </div>
