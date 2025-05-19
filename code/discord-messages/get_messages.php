c0a2d23b85c373dcedcae598828601717f0ce98c
Dylan <dylan.moulin9272@gmail.com>
2025-05-19 12:26:27 +0200

    Limite de l'affichage des messages supprimée

--- code/discord-messages/get_messages.php
+++ code/discord-messages/get_messages.php
@@ -2,7 +2,7 @@
 require_once '../bdd.php';
 $channel_id = $_GET['channel_id'];
 
-$stmt = $conn->prepare("SELECT message_content FROM channel_messages WHERE channel_id = :channel_id ORDER BY timestamp DESC LIMIT 20");
+$stmt = $conn->prepare("SELECT message_content FROM channel_messages WHERE channel_id = :channel_id ORDER BY timestamp DESC");
 $stmt->bindParam(":channel_id", $channel_id);
 $stmt->execute();
 $rows = $stmt->fetchAll();
